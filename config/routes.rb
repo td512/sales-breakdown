2047f3cc800b945ca010af9c3daaa650b94df99d
Theo Morra <theo.morra@m6securities.com>
2020-08-24 23:48:37 +1200

    adds new root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'public#index'
   namespace :api do
     namespace :v1 do
       resources :transactions, only: [:index, :show], defaults: {format: :json}, :constraints => {:id => /[^\/]+/}
